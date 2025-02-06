6344a48c4ec6d6a20e84bf2cfd861188db97dfe9
daniehh29 <daniehh29@gmail.com>
2025-02-06 10:56:25 +0100

    Se ha añadido el guardado de las coordenadas en un archivo csv

--- navegacion_autonoma/planificacion_ruta.py
+++ navegacion_autonoma/planificacion_ruta.py
@@ -1,22 +1,28 @@
 # import libraries
 import googlemaps
 import folium
+import csv
 
 # prepare API
-gmaps = googlemaps.Client(key="AIzaSyBEZvL3tGskyLHol63YZ4-z39AxAZuPgBI")
+gmaps = googlemaps.Client(key="API-key")
+# eps 3 entrada eps 2
+# (38.38717735745423, -0.5122577483285744)
+# eps 3 entrada eps 1
+# (38.38669284754157, -0.5118640102682578)
 
-# eps 1
-origin = (38.386884064791204, -0.511243216267247)
-# club social 1
-destiny = (38.384541941033376, -0.5162267605152372)
+origin = (38.38717735745423, -0.5122577483285744)
+destiny = (38.38669284754157, -0.5118640102682578)
 
 # get route
 route = gmaps.directions(origin, destiny, mode="walking")
 
+coordenadas = []
 
 if route:
     # draw map
-    map = folium.Map(location=origin, zoom_start=13)
+    map = folium.Map(location=origin, zoom_start=25)
+
+    coordenadas.append(origin)
 
     # add the origin marker to the map
     marker = folium.Marker(location=origin).add_to(map)
@@ -30,6 +36,12 @@ if route:
         marker = folium.Marker(location=(step["end_location"]["lat"], step["end_location"]["lng"])).add_to(map)
         # add the line for the route segment connecting current to next step into the map
         line = folium.PolyLine(locations=[(step["start_location"]["lat"], step["start_location"]["lng"]), (step["end_location"]["lat"], step["end_location"]["lng"])]).add_to(map)
+        coordenadas.append((step["end_location"]["lat"], step["end_location"]["lng"]))
 
+    # save the coordinates
+    with open("ruta_eps3_corta.csv", "w", newline="") as file:
+        writer = csv.writer(file)
+        for coord in coordenadas:
+            writer.writerow(coord)
     # save the map
-    map.save("map.html")
+    map.save("ruta_eps3_corta.html")
